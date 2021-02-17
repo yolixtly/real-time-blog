4a39caf2e6595930e05181b2e06a0cffe87cf731
yolixtly <yolixtly_13@hotmail.com>
2021-02-17 16:53:09 -0700

    amplify config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+
+Amplify.configure(aws_exports);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
