8b9c1d6ea55fb206039c3b7f218477cb384b19e9
yolixtly <yolixtly_13@hotmail.com>
2021-02-18 12:59:27 -0700

    edit button component

---
+++ src/components/EditPost.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+
+class EditPost extends Component {
+    render() {
+        return (
+            <button>
+                Edit
+            </button>
+        )
+    }
+}
+
+export default EditPost;
