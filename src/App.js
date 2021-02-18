86cdd52c45b8afd55d4bfe07a07bc7ae384abc60
yolixtly <yolixtly_13@hotmail.com>
2021-02-18 12:59:44 -0700

    create post component posting to dynamodb

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import DisplayPost from './components/DisplayPosts'
+import CreatePost from './components/CreatePost'
 
 function App() {
   return (
     <div className="App">
+      <CreatePost />
       <DisplayPost />
     </div>
   );
---
+++ src/components/CreatePost.js
@@ -0,0 +1,77 @@
+import { API, graphqlOperation } from 'aws-amplify';
+import React, { Component } from 'react';
+import { createPost } from '../graphql/mutations'
+
+
+class CreatePost extends Component {
+    state = {
+        postOwnerId: "",
+        postOwnerUsername: "",
+        postTitle: "",
+        postBody: ""
+    }
+    componentDidMount = async () => {
+        //TOdo
+    }
+
+    handleChangePost = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    handleAddPost = async (event) => {
+        event.preventDefault();
+
+        const input = {
+            postOwnerId: "yolixtlyId",
+            // postOwnerId: this.state.postOwnerId,
+            postOwnerUsername: "yolixtly",
+            // postOwnerUsername: this.state.postOwnerUsername,
+            postTitle: this.state.postTitle,
+            postBody: this.state.postBody,
+            createdAt: new Date().toISOString()
+        }
+
+        console.log(input);
+        await API.graphql(graphqlOperation(createPost, { input }))
+
+        this.setState({
+            postTitle: "",
+            postBody: "",
+        })
+    }
+    render() {
+        return (
+            <form className="add-post"
+                onSubmit={this.handleAddPost} >
+
+                <input style={{ font: '19px' }}
+                    type="text" placeholder="Title"
+                    name="postTitle"
+                    required
+                    value={this.state.postTitle}
+                    onChange={this.handleChangePost}
+                />
+
+                <textarea
+                    type="text"
+                    name="postBody"
+                    rows="3"
+                    cols="40"
+                    required
+                    placeholder="New Blog Post"
+                    value={this.state.postBody}
+                    onChange={this.handleChangePost}
+                />
+
+                <input type="submit"
+                    className="btn"
+                    style={{ fontSize: '19px' }} />
+
+
+            </form>
+        )
+    }
+}
+
+export default CreatePost;
