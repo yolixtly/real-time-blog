9ddf95dea2ad0a53103074e852f542fa890c496c
yolixtly <yolixtly_13@hotmail.com>
2021-02-17 17:29:09 -0700

    basic display post component

--- src/components/displayPosts.js
+++ src/components/displayPosts.js
@@ -3,6 +3,9 @@ import { listPosts } from '../graphql/queries';
 import { API, graphqlOperation } from 'aws-amplify';
 
 class DisplayPost extends Component {
+    state = {
+        posts: []
+    }
     componentDidMount = async () => {
         this.getPosts();
     }
@@ -10,16 +13,40 @@ class DisplayPost extends Component {
     getPosts = async () => {
         const result = await API.graphql(graphqlOperation(listPosts))
 
+        this.setState({
+            posts: result.data.listPosts.items
+        })
         console.log(`All Posts: ${JSON.stringify(result.data.listPosts.items)}`)
         console.log(result.data.listPosts.items)
     }
     render() {
-        return (
-            <div className="App">
-                Hello World
-            </div>
-        )
+        const { posts } = this.state;
+        return posts.map((post) => {
+            return (
+                <div key={post.id} className="posts" style={rowStyle}>
+                    <h1>{post.postTitle}</h1>
+                    <h4>
+                        {post.postBody}
+                    </h4>
+                    <span>
+                        <time style={{ fontStyle: "italic", color: "#0ca5e297" }}>
+                            {"Wrote by: "} {post.postOwnerUsername}
+                            {" on "}
+                            {" "}
+                            {new Date(post.createdAt).toDateString()}
+                        </time>
+                    </span>
+                </div >
+            )
+        })
     }
+}
+
+const rowStyle = {
+    background: '#f4f4f4',
+    padding: '10px',
+    border: '1px #ccc dotted',
+    margin: '14px'
 }
 
 export default DisplayPost;
